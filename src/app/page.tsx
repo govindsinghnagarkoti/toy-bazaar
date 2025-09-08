f48a7bc183b6f3ba65d2d6907ae9b2cc682cd6c0
Govind Singh <gosingh@ex2india.com>
2025-09-08 17:55:18 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,12 @@
-import Link from "next/link";
-import ToysListPage from "./toys/page";
+"use client";
+
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const ToysListPage = dynamic(() => import("./toys/page"), { ssr: false });
 
 export default function Home() {
-  return (
-    // <main className="min-h-screen bg-gradient-to-br from-pink-300 via-blue-300 to-blue-600 text-white flex flex-col items-center justify-center px-6">
-    //   <h1 className="text-5xl md:text-7xl font-extrabold text-center leading-tight drop-shadow-lg">
-    //     Welcome to <span className="text-white">Toy Bazaar</span>
-    //   </h1>
-    //   <p className="mt-6 text-lg md:text-xl text-center max-w-xl text-white">
-    //     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptas.
-    //   </p>
-    //   <div className="mt-10 flex gap-4">
-    //     <Link
-    //       href="/toys"
-    //       className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-lg transition"
-    //     >
-    //       Get Started
-    //     </Link>
-    //     <Link
-    //       href="/about "
-    //       className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-lg transition"
-    //     >
-    //       Connect With Us
-    //     </Link>
-    //   </div>
-    // </main>
-     <ToysListPage/>
-
-  );
+  return (<Suspense  fallback={<div>Loading...</div>}>
+    <ToysListPage />
+  </Suspense>);
 }
